99aedb360f8ea67efcb0fa36129a26b9c83f20c3
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-05 23:57:00 +0300

    fix 6-final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((values) => {
-      console.log(values);
+      values.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }));
     });
 }
