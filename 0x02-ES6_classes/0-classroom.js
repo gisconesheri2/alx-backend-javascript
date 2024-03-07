9deb484545095ed18b4797be51f9d5f86c32ddcf
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-07 22:12:41 +0300

    first classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-underscore-dangle */
+export default class ClassRoom {
+  constructor(maxSidentsSize) {
+    this._maxStudentsSize = maxSidentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof (code) !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof (val) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (typeof (val) !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = val;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
