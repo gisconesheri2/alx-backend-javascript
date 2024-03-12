6045affb263f5afe6863a0c1b1e3e0a8c2aa3825
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-12 14:03:19 +0300

    implement weakMap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+    if (!weakMap.has(endpoint)) {
+        weakMap.set(endpoint, 0);
+      }
+    let numCalled = weakMap.get(endpoint);
+    if (numCalled >= 5) {
+        throw new Error('Endpoint load is high')
+    } else {
+        numCalled = numCalled + 1;
+        weakMap.set(endpoint, numCalled);
+    }
+}
+
+export {weakMap, queryAPI};
--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,3 +1,15 @@
-import cleanSet from './8-clean_set.js';
+import { queryAPI, weakMap } from "./100-weak.js";
 
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana'])));
+const endpoint = { protocol: 'http', name: 'getUsers' };
+weakMap.get(endpoint);
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
