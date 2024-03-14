046fc57f21ea373d87d92225c69847f3b6e03e25
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-14 23:09:30 +0300

    fix count error in 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,10 +2,10 @@ const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
+    weakMap.set(endpoint, 0);
   }
   let numCalled = weakMap.get(endpoint);
-  if (numCalled >= 5) {
+  if (numCalled >= 4) {
     throw new Error('Endpoint load is high');
   } else {
     numCalled += 1;
--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -4,10 +4,10 @@ const endpoint = { protocol: 'http', name: 'getUsers' };
 weakMap.get(endpoint);
 
 queryAPI(endpoint);
-// console.log(weakMap.get(endpoint));
+console.log(weakMap.get(endpoint));
 
 queryAPI(endpoint);
-// console.log(weakMap.get(endpoint));
+console.log(weakMap.get(endpoint));
 
 queryAPI(endpoint);
 queryAPI(endpoint);
